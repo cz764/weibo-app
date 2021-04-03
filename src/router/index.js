209ab69afadb252b8633f74936c98923119fdc4f
Chen Zhu <cz764@nyu.edu>
2021-04-03 13:09:54 -0400

    Add post new weibo, textArea and handle scroll full size input

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import React, { lazy, Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 
 const Home = lazy(() => import("../pages/Home"));
+const New = lazy(() => import("../pages/New"));
 
 const Router = () => {
   return (
     <Suspense fallback="Loading...">
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/new" component={New} />
       </Switch>
     </Suspense>
   );
