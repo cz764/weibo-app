45748da6185d17daf4cd81a1f9ae2acccfc7d9c6
Chen Zhu <cz764@nyu.edu>
2021-04-02 20:27:53 -0400

    Add top bar, top bar links and styles

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,7 @@
 export const ACCESS_TOKEN = "";
+
+export const APP_KEY = "3696852244";
+export const APP_SECRET = "";
+export const REDIRECT_URI = encodeURIComponent("http://baidu.com:3000/login");
+
+export const LOGIN_URL = `https://demo.don.red/weibo/api/oauth2/authorize?client_id=${APP_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=email`;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useDispatch, useMappedState } from "redux-react-hook";
 import InfiniteScroll from "react-infinite-scroller";
+import { Row, Affix } from "antd";
+import { Link } from "react-router-dom";
+import { UserOutlined, EditOutlined } from "@ant-design/icons";
 import { getHomeTimeline } from "../../actions/timeline";
 import Post from "./components/Post";
 import styles from "./index.module.scss";
+import { LOGIN_URL } from "../../constants";
 
 const mapStateTimeline = (state) => state.timeline;
 
@@ -17,6 +21,17 @@ const Home = () => {
 
   return (
     <div className={styles.container}>
+      <Affix offsetTop={0}>
+        <Row className={styles.appbar} justify="space-between" align="middle">
+          <Link to={LOGIN_URL}>
+            <UserOutlined className={styles.icon} />
+          </Link>
+          <div className={styles.appTitle}>Weibo App</div>
+          <Link to="/new">
+            <EditOutlined className={styles.icon} />
+          </Link>
+        </Row>
+      </Affix>
       <InfiniteScroll
         initialLoad
         pageStart={1}
